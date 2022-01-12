b3ace5acb965e7cbddae99f3f2718d019975e120
Artem Boyarskov <arboyarskov@ya.ru>
2022-01-12 15:54:07 +0300

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -4,21 +4,27 @@ public class Cat {
 
     private String food;
 
+    private String name;
+
     public void show() {
-        System.out.println(this.food);
+        System.out.println("cat " + this.name + " eats : " + this.food);
     }
 
     public void eat(String meal) {
         this.food = meal;
     }
 
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
     public static void main(String[] args) {
-        System.out.println("There are gav's food.");
         Cat gav = new Cat();
+        gav.giveNick("Gav");
         gav.eat("kotleta");
         gav.show();
-        System.out.println("There are black's food.");
         Cat black = new Cat();
+        black.giveNick("Black");
         black.eat("fish");
         black.show();
     }
