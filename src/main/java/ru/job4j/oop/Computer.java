6711ca7fc2ece7325cf4898ab68aa7e9e6076b60
Artem Boyarskov <arboyarskov@ya.ru>
2022-01-14 11:55:37 +0300

    1.7. Конструктор [#173889]

--- src/main/java/ru/job4j/oop/Computer.java
+++ src/main/java/ru/job4j/oop/Computer.java
@@ -6,16 +6,16 @@ public class Computer {
     private int ssd;
     private String cpu;
 
-    public void printInfo() {
-        System.out.println("Много мониторов: " + multiMonitor);
-        System.out.println("SSD: " + ssd + " GB");
-        System.out.println("Модель CPU: " + cpu);
-    }
-
     public Computer(boolean multiMonitor, int ssd, String cpu) {
         this.multiMonitor = multiMonitor;
         this.ssd = ssd;
         this.cpu = cpu;
+    }
+
+    public void printInfo() {
+        System.out.println("Много мониторов: " + multiMonitor);
+        System.out.println("SSD: " + ssd + " GB");
+        System.out.println("Модель CPU: " + cpu);
     }
 
     public static void main(String[] args) {
