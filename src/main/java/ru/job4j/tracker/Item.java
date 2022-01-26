d0fa6dbd5ea5ba7f47cfd5312420d6466dd4a961
Artem Boyarskov <arboyarskov@ya.ru>
2022-01-26 19:58:36 +0300

    9. toString [#310018 #256193]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,7 +1,6 @@
 package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
-import java.util.Formatter;
 import java.time.format.DateTimeFormatter;
 
 public class Item {
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,7 +1,5 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
-
 public class StartUI extends Item {
 
     public static void main(String[] args) {
