ca7b5d036ecc36ebb949bd23c3b684f89603982a
Artem Boyarskov <arboyarskov@ya.ru>
2022-01-26 17:45:49 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #255457]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -61,6 +61,7 @@ public class Tracker {
         int index = indexOf(id);
         if (index != -1) {
         System.arraycopy(items, index + 1, items, index, size - index - 1);
+        items[size - 1] = null;
         size--;
         }
         return index != -1;
