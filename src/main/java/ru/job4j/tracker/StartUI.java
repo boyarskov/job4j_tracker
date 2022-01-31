00f4609cea31f09574517dddb1ba9b3fdce0021a
Artem Boyarskov <arboyarskov@ya.ru>
2022-01-31 09:59:57 +0300

    2.2. Реализация класса StartUI. Вывод меню [#500743 #258141]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,7 +8,7 @@ public class StartUI {
         boolean run = true;
         while (run) {
             showMenu();
-            System.out.println("Select: ");
+            System.out.print("Select: ");
             int select = Integer.parseInt(scanner.nextLine());
             if (select != 6) {
                 System.out.println("Пользователь выбрал: " + select);
