9483a120263330d7ed6a05947b122f218eeb8ee9
Artem Boyarskov <arboyarskov@ya.ru>
2022-02-07 13:14:01 +0300

    7. Шаблон проектирования - Стратегия [#786]

---
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -0,0 +1,13 @@
+package ru.job4j.strategy;
+
+public class Square implements Shape {
+
+    @Override
+    public String draw() {
+        String ln = System.lineSeparator();
+        return    " _______ " + ln
+                + "|       |" + ln
+                + "|       |" + ln
+                + "|_______|" + ln;
+    }
+}
