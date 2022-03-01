13c10fa4fedf226ae83952852fdd62f93e3697fc
Artem Boyarskov <arboyarskov@ya.ru>
2022-03-01 23:52:43 +0300

    1. Телефонный справочник на базе ArrayList [#41598]

---
+++ src/main/java/ru/job4j/collection/PhoneDictionary.java
@@ -0,0 +1,19 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        return result;
+    }
+
+
+
+}
