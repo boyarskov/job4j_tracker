0d87746968e973ef7b8f65d487414f6cacd14508
Artem Boyarskov <arboyarskov@ya.ru>
2022-03-12 23:16:23 +0300

    2. Очередь с приоритетом на LinkedList [#41670 #272013]28

--- src/main/java/ru/job4j/collection/PriorityQueue.java
+++ src/main/java/ru/job4j/collection/PriorityQueue.java
@@ -8,9 +8,10 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (task.getPriority() > element.getPriority()) {
-                index++;
+            if (task.getPriority() < element.getPriority()) {
+                break;
             }
+            index++;
         }
         this.tasks.add(index, task);
     }
--- src/test/java/ru/job4j/collection/PriorityQueueTest.java
+++ src/test/java/ru/job4j/collection/PriorityQueueTest.java
@@ -2,10 +2,6 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
