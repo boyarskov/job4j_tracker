8510486d9274eb2a6d4e7ff63395916bff41a052
Artem Boyarskov <arboyarskov@ya.ru>
2022-03-30 17:04:03 +0300

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/collection/ListSort.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ListSort {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(5, 3, 4, 1, 2);
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+    }
+}
