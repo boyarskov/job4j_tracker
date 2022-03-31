1477d10a3710368276ec2a36395cce8bebaa07eb
Artem Boyarskov <arboyarskov@ya.ru>
2022-03-31 09:40:01 +0300

    1. Организовать сортировку User [#10034]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -14,7 +14,8 @@ public class User implements Comparable<User> {
 
     @Override
     public int compareTo(User o) {
-        return (this.name.compareTo(o.name)) == 0 ? Integer.compare(this.age, o.age) : (this.name.compareTo(o.name));
+        int rsl = this.name.compareTo(o.name);
+        return rsl == 0 ? Integer.compare(this.age, o.age) : rsl;
     }
 
     @Override
