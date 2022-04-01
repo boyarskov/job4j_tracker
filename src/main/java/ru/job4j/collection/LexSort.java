26e8679e4c6a76bccdffad7f22329f246712f82d
Artem Boyarskov <arboyarskov@ya.ru>
2022-04-01 23:39:14 +0300

    4. Сортировка номера [#221284]

---
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -0,0 +1,11 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+
+    @Override
+    public int compare(String left, String right) {
+        return left.compareTo(right);
+    }
+}
