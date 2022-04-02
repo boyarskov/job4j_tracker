65b1a1d153e12327ceb9a671b52aa4261e031478
Artem Boyarskov <arboyarskov@ya.ru>
2022-04-02 18:07:48 +0300

    2. Что такое JavaDoc [#313879]

--- src/main/java/ru/job4j/collection/PriorityQueue.java
+++ src/main/java/ru/job4j/collection/PriorityQueue.java
@@ -2,9 +2,24 @@ package ru.job4j.collection;
 
 import java.util.LinkedList;
 
+/**
+ * Класс описывает работу простейшей очереди по приоритету, которая работает
+ * по принципу FIFO (first in - first out)
+ * @author Artem Boyarskov
+ * @version 1.0
+ */
 public class PriorityQueue {
+    /**
+     * Хранение задания осуществляется в коллекции типа LinkedList
+     */
     private LinkedList<Task> tasks = new LinkedList<>();
 
+    /**
+     * Метод принимает на вход заявку и добавляет ее в очередь.
+     * Если встречаются 2 задания с одинаковым приоритетом, то в очереди
+     * они распределяются по принципу FIFO
+     * @param task
+     */
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
@@ -16,6 +31,10 @@ public class PriorityQueue {
         this.tasks.add(index, task);
     }
 
+    /**
+     * Метод позволяет получить первую задачу в очереди
+     * @return возвращает задачу из головы очереди или null, если очередь пуста
+     */
     public Task take() {
         return tasks.poll();
     }
