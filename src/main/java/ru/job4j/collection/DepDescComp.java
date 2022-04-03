2152a6d48bd73ec01d5286eb7f7c3d47be3b14d9
Artem Boyarskov <arboyarskov@ya.ru>
2022-04-04 01:25:28 +0300

    2. Отсортировать департаменты [#34352]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -3,18 +3,18 @@ package ru.job4j.collection;
 import java.util.Comparator;
 /**
  * Компаратор для сортировки по убыванию
+ * DepDescComp расширяет интерфейс Comparator
  */
-/*
-Класс DepDescComp должен расширять интерфейс Comparator:
-- метод compare() должен реализовать сравнение в соответствии с описанием.
- */
+
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        return 0;
+        String[] one = o1.split("/");
+        String[] two = o2.split("/");
+        int rsl = two[0].compareTo(one[0]);
+        if (rsl == 0) {
+            rsl = o1.compareTo(o2);
+        }
+        return rsl;
     }
-
-    /*
-    После реализации методов в классе Departments проверить корректность реализации вы можете с помощью тестов: DepartmentsTest
-     */
 }
--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -1,36 +1,39 @@
 package ru.job4j.collection;
 
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Departments {
-/*
- метод fillGaps() должен сформировать список строк, при этом требуется учесть все возможно отсутствующие коды подразделений;
+/**
+ * метод fillGaps() должен сформировать список строк, при этом учитывает отсутствующие коды подразделений;
  */
     public static List<String> fillGaps(List<String> deps) {
         Set<String> tmp = new LinkedHashSet<>();
         for (String value : deps) {
             String start = "";
             for (String el : value.split("/")) {
-                /* tmp.add start + "/" + el */
+                if (start.equals("")) {
+                    start = el;
+                } else {
+                    start += "/" + el;
+                }
+                tmp.add(start);
             }
         }
         return new ArrayList<>(tmp);
     }
 
-/*
-метод sortAsc() должен просто отсортировать по возрастанию список строк, который он принимает на вход,
-вызывать дополнительно fillGaps() в этом методе не нужно;
+/**
+ * метод sortAsc() отсортировывает по возрастанию список строк, который он принимает на вход
  */
     public static void sortAsc(List<String> orgs) {
-    }
+        Collections.sort(orgs);
+            }
 
-    /*
-метод sortDesc() должен просто отсортировать в соответствии с компаратором DepDescComp список строк,
-который он принимает на вход, вызывать дополнительно fillGaps() в этом методе не нужно;
+    /**
+     * метод sortDesc() отсортировывает в соответствии с компаратором DepDescComp список строк,
+     * который он принимает на вход
      */
     public static void sortDesc(List<String> orgs) {
+        Collections.sort(orgs, new DepDescComp());
     }
 }
