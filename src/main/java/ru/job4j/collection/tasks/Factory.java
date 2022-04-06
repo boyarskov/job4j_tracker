31f393b1fc02f0a152457ea631460e64b1b667a0
Artem Boyarskov <arboyarskov@ya.ru>
2022-04-06 17:15:08 +0300

    0.5. Заполнение списка фабричным методом

---
+++ src/main/java/ru/job4j/collection/tasks/Factory.java
@@ -0,0 +1,12 @@
+package ru.job4j.collection.tasks;
+
+import java.util.List;
+
+public class Factory {
+    public static void main(String[] args) {
+        List<String> list = List.of("first", "second", "third", "fourth", "fifth");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+}
