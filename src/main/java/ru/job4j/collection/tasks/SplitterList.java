8fb5fbdda3c08d5a2f2107e0b831a40c2e27be68
Artem Boyarskov <arboyarskov@ya.ru>
2022-04-09 00:14:57 +0300

    0.8. Удаление одного списка из другого

--- src/main/java/ru/job4j/collection/tasks/SplitterList.java
+++ src/main/java/ru/job4j/collection/tasks/SplitterList.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 public class SplitterList {
     public static List<String> split(List<String> left, List<String> middle, List<String> right) {
-        left.addAll(middle);
         left.retainAll(middle);
+        left.removeAll(right);
         return left;
     }
 }
