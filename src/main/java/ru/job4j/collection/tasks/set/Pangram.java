77cd124c89d920684da9947373577b1821e7352b
Artem Boyarskov <arboyarskov@ya.ru>
2022-05-02 23:34:10 +0300

    0.3. Панграмма (1.3.2. Коллекция Set, HashSet)

--- src/main/java/ru/job4j/collection/tasks/set/Pangram.java
+++ src/main/java/ru/job4j/collection/tasks/set/Pangram.java
@@ -2,28 +2,21 @@ package ru.job4j.collection.tasks.set;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.Arrays;
+import java.util.*;
 
 public class Pangram {
-    public static boolean checkString(String s) {
-        s = s.replaceAll("\\s+", "");
-        s = s.toLowerCase();
-        Set<String> set = new HashSet<>(Arrays.asList(s.split("")));
-        boolean rsl = false;
-        if (set.size() == 26) {
-                if ((set.contains(String.valueOf('a'))) || (set.contains(String.valueOf('b')))
-                        || (set.contains(String.valueOf('c'))) || (set.contains(String.valueOf('d'))) || (set.contains(String.valueOf('e')))
-                        || (set.contains(String.valueOf('f'))) || (set.contains(String.valueOf('g'))) || (set.contains(String.valueOf('h')))
-                        || (set.contains(String.valueOf('i'))) || (set.contains(String.valueOf('j'))) || (set.contains(String.valueOf('k')))
-                        || (set.contains(String.valueOf('l'))) || (set.contains(String.valueOf('m'))) || (set.contains(String.valueOf('n')))
-                        || (set.contains(String.valueOf('o'))) || (set.contains(String.valueOf('p'))) || (set.contains(String.valueOf('q')))
-                        || (set.contains(String.valueOf('r'))) || (set.contains(String.valueOf('s'))) || (set.contains(String.valueOf('t')))
-                        || (set.contains(String.valueOf('u'))) || (set.contains(String.valueOf('v'))) || (set.contains(String.valueOf('w')))
-                        || (set.contains(String.valueOf('x'))) || (set.contains(String.valueOf('y'))) || (set.contains(String.valueOf('z')))) {
-                    rsl = true;
-                }
-            }
-            return rsl;
+    public static boolean checkString(String str) {
+        int count = 0;
+        char s[] = str.replaceAll("\\s+", "").toCharArray();
+        Set<Character> hs = new HashSet<Character>();
+        for (int i = 0; i < s.length; i++) {
+            hs.add(s[i]);
         }
+        Iterator<Character> i = hs.iterator();
+        while (i.hasNext()) {
+            count++;
+            i.next();
+        }
+        return count == 26;
+    }
 }
-
