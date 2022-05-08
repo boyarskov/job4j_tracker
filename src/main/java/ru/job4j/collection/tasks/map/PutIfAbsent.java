4917d4978c39c0f384b7f5b0d8e6983a273070bf
Artem Boyarskov <arboyarskov@ya.ru>
2022-05-08 14:39:25 +0300

    1.2. Добавление элемента в отображение без проверки. (1.3.3. Map. HashMap. LinkedHashMap)

---
+++ src/main/java/ru/job4j/collection/tasks/map/PutIfAbsent.java
@@ -0,0 +1,49 @@
+package ru.job4j.collection.tasks.map;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class PutIfAbsent {
+
+    private final Map<Integer, User> users;
+
+    public PutIfAbsent(Map<Integer, User> users) {
+        this.users = users;
+    }
+
+    public boolean addNewElement(User u) {
+        users.putIfAbsent(u.getId(), u);
+        return users.containsValue(u);
+    }
+
+    public static class User {
+        private int id;
+        private String name;
+
+        public User(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            User user = (User) o;
+            return Objects.equals(name, user.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+    }
+}
---
+++ src/test/java/ru/job4j/collection/tasks/map/PutIfAbsentTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.collection.tasks.map;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+import ru.job4j.collection.tasks.map.PutIfAbsent.User;
+
+public class PutIfAbsentTest {
+    @Test
+    public void addNewElement() {
+        Map<Integer, User> map = new HashMap<>(
+                Map.of(
+                        1, new User(1, "Name1"),
+                        2, new User(2, "Name2")
+                )
+        );
+        PutIfAbsent put = new PutIfAbsent(map);
+        User user = new User(1, "Name3");
+        boolean rsl = put.addNewElement(user);
+        assertThat(rsl, is(false));
+    }
+
+    @Test
+    public void addNewElementTrue() {
+        Map<Integer, User> map = new HashMap<>(
+                Map.of(
+                        1, new User(1, "Name1"),
+                        2, new User(2, "Name2")
+                )
+        );
+        PutIfAbsent put = new PutIfAbsent(map);
+        User user = new User(3, "Name3");
+        boolean rsl = put.addNewElement(user);
+        assertThat(rsl, is(true));
+    }
+}
