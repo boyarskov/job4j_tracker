b4fae430dc785ba254e209739ee90596f507f603
Artem Boyarskov <arboyarskov@ya.ru>
2022-06-05 11:04:29 +0300

    1.2. Функциональный интерфейс [#247497 #309079]

--- src/main/java/ru/job4j/lambda/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/lambda/FunctionalInterfaces.java
@@ -14,12 +14,7 @@ public class FunctionalInterfaces {
         b.accept(5, "five");
         b.accept(6, "six");
         b.accept(7, "seven");
-        BiPredicate<Integer, String> biPred = (i, s) -> {
-                if (i % 2 == 0 || map.get(i).length() == 4) {
-                    return true;
-                }
-                return false;
-            };
+        BiPredicate<Integer, String> biPred = (i, s) -> i % 2 == 0 || map.get(i).length() == 4;
         for (Integer i : map.keySet()) {
            if (biPred.test(i, map.get(i))) {
                 System.out.println("key: " + i + " value: " + map.get(i));
