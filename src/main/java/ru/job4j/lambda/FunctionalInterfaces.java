871a9f4daa12b3d918235544a9c7aa788d6e5746
Artem Boyarskov <arboyarskov@ya.ru>
2022-06-05 10:37:52 +0300

    1.2. Функциональный интерфейс [#247497 #309079]

--- src/main/java/ru/job4j/lambda/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/lambda/FunctionalInterfaces.java
@@ -1,54 +1,36 @@
 package ru.job4j.lambda;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.*;
 
 public class FunctionalInterfaces {
     public static void main(String[] args) {
-        //1
         Map<Integer, String> map = new HashMap<>();
-        map.put(1, "one");
-        map.put(2, "two");
-        map.put(3, "three");
-        map.put(4, "four");
-        map.put(5, "five");
-        map.put(6, "six");
-        map.put(7, "seven");
-        //System.out.println(map);
-        //made
-        BiConsumer<String, Integer> b = (s, s1) -> map.put(s1, s);
-        //2
-        //BiPredicate<Integer, String> biPred = (i, s) -> {
-            Predicate<Integer> biPred = (i) -> {
+        BiConsumer<Integer, String> b = (i, s) -> map.put(i, s);
+        b.accept(1, "one");
+        b.accept(2, "two");
+        b.accept(3, "three");
+        b.accept(4, "four");
+        b.accept(5, "five");
+        b.accept(6, "six");
+        b.accept(7, "seven");
+        BiPredicate<Integer, String> biPred = (i, s) -> {
                 if (i % 2 == 0 || map.get(i).length() == 4) {
                     return true;
                 }
                 return false;
             };
         for (Integer i : map.keySet()) {
-           if (biPred.test(i)) {
+           if (biPred.test(i, map.get(i))) {
                 System.out.println("key: " + i + " value: " + map.get(i));
             }
         }
-        //3
-/*
-            Заменить создание ArrayList из значений Map на Supplier, объявлен ниже, требуется его реализовать.
-         */
-        Supplier<List<String>> sup = ...;
-        List<String> strings = new ArrayList<>(map.values());
-
-        Consumer<String> con = ...;
-        Function<String, String> func = ...;
+        Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
+        List<String> strings = sup.get();
+        Consumer<String> con = (s) -> System.out.println(s);
+        Function<String, String> func = s -> s.toUpperCase();
         for (String s : strings) {
-            /*
-                Заменить вывод строк на применение Consumer
-                Заменить преобразование строк к строкам в верхнем регистре с помощью Function
-                Необходимое объявлено выше, требуется их реализовать.
-            */
-            System.out.println(s.toUpperCase());
+            con.accept(func.apply(s));
         }
     }
 }
