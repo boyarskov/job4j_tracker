cbb961c843ae2d8137de9f34dd07c6cf2fc616a6
Artem Boyarskov <arboyarskov@ya.ru>
2022-06-06 20:09:23 +0300

    1.3. Встроенные функциональные интерфейсы. [#249209]019

---
+++ src/main/java/ru/job4j/lambda/SearchFolder.java
@@ -0,0 +1,17 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SearchFolder {
+    public static List<Folder> filter(List<Folder> list, Predicate<Folder> pred) {
+        List<Folder> rsl = new ArrayList<>();
+        for (Folder f : list) {
+            if (pred.test(f)) {
+                rsl.add(f);
+            }
+        }
+       return rsl;
+    }
+}
