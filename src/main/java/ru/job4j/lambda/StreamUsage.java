c3bacdf6891643bb13c7cbb6d93543c9b278c27f
Artem Boyarskov <arboyarskov@ya.ru>
2022-06-17 21:25:41 +0300

    0.1. Отладка лямбда в IDEA [#259165]

--- src/main/java/ru/job4j/lambda/StreamUsage.java
+++ src/main/java/ru/job4j/lambda/StreamUsage.java
@@ -1,7 +1,6 @@
 package ru.job4j.lambda;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class StreamUsage {
     public static class Task {
@@ -12,40 +11,18 @@ public class StreamUsage {
             this.name = name;
             this.spent = spent;
         }
-
-        @Override
-        public String toString() {
-            return "Task{"
-                    + "name='" + name + '\''
-                    + ", spent=" + spent
-                    + '}';
-        }
     }
 
     public static void main(String[] args) {
         List<Task> tasks = List.of(
-                new Task("Bug #1", 100),
-                new Task("Task #2", 100),
-                new Task("Bug #3", 100)
+                new Task("Bug #1", 10),
+                new Task("Task #2", 20),
+                new Task("Bug #3", 40)
         );
-        List<Task> bugs = tasks.stream().filter(
-                task -> task.name.contains("Bug")
-        ).collect(Collectors.toList());
-        bugs.forEach(System.out::println);
-        System.out.println("---");
-        List<String> names = tasks.stream().map(
-                task -> task.name
-        ).collect(Collectors.toList());
-        names.forEach(System.out::println);
-        System.out.println("---");
-        long total = tasks.stream().map(
-                task -> task.spent
-        ).reduce(0L, Long::sum);
-        System.out.println("Total time: " + total);
-        System.out.println("---");
-        List<Task> allBugs = tasks.stream().filter(
-                task -> task.name.contains("Bug")
-        ).collect(Collectors.toList());
-        allBugs.forEach(System.out::println);
+        tasks.stream()
+                .filter(task -> task.name.contains("Bug"))
+                .filter(task -> task.spent > 30)
+                .map(task -> task.name + " " + task.spent)
+                .forEach(System.out::println);
     }
 }
