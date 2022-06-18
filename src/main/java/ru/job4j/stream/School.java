161b776006972aeddd722bdd4f711962c3d9ae40
Artem Boyarskov <arboyarskov@ya.ru>
2022-06-18 12:44:55 +0300

    1. Фильтрация учеников [#110058]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -6,6 +6,8 @@ import java.util.stream.Collectors;
 
 public class School {
     public List<Student> collect(List<Student> students, Predicate<Student> predict) {
-        return students.stream().filter(predict).collect(Collectors.toList());
+        return students.stream()
+                .filter(predict)
+                .collect(Collectors.toList());
     }
 }
