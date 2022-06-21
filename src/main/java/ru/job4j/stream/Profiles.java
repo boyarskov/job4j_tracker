2aeeac21ad1516aa129906b851aeda3732d05be8
Artem Boyarskov <arboyarskov@ya.ru>
2022-06-21 19:57:06 +0300

    2. Список адресов [#110062 #314743]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
        return profiles.stream()
-                .map(profile -> profile.getAddress())
+                .map(Profile::getAddress)
                 .collect(Collectors.toList());
     }
 
