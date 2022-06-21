d92f1c97091f7b18c63b37d47825858131c1446c
Artem Boyarskov <arboyarskov@ya.ru>
2022-06-21 20:57:27 +0300

    5. Преобразование матрицы чисел в список чисел. [#110228]

---
+++ src/main/java/ru/job4j/stream/MatrixToList.java
@@ -0,0 +1,13 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MatrixToList {
+    public static List<Integer> convert(Integer[][] matrix) {
+        return Stream.of(matrix)
+                .flatMap(e -> Stream.of(e))
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/ru/job4j/steam/MatrixToListTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.steam;
+
+import org.junit.Test;
+import ru.job4j.stream.MatrixToList;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MatrixToListTest {
+    @Test
+    public void whenInputSquareMatrix() {
+        Integer[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        List<Integer> result = MatrixToList.convert(matrix);
+        List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenInputNoSquareMatrix() {
+        Integer[][] matrix = {
+                {1},
+                {2, 3},
+                {4, 5, 6}
+        };
+        List<Integer> result = MatrixToList.convert(matrix);
+        List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6);
+        assertEquals(expected, result);
+    }
+    }
