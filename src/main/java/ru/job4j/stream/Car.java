c64de58aeb43a2e99108a85b1c4d3a8e33caa577
Artem Boyarskov <arboyarskov@ya.ru>
2022-06-24 21:08:56 +0300

    8. Шаблоны проектирования в Stream API [#310012]

---
+++ src/main/java/ru/job4j/stream/Car.java
@@ -0,0 +1,87 @@
+package ru.job4j.stream;
+
+import java.time.LocalDate;
+
+public class Car {
+    private String brand;
+
+    private String model;
+
+    private LocalDate created;
+
+    private double volume;
+
+    private String color;
+
+    static class Builder {
+        private String brand;
+        private String model;
+        private LocalDate created;
+        private double volume;
+        private String color;
+
+        Builder buildBrand(String brand) {
+            this.brand = brand;
+            return this;
+        }
+
+        Builder buildModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        Builder buildCreated(LocalDate created) {
+            this.created = created;
+            return this;
+        }
+
+        Builder buildVolume(double volume) {
+            this.volume = volume;
+            return this;
+        }
+
+        Builder buildColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        Car build() {
+            Car infoCar = new Car();
+            infoCar.brand = brand;
+            infoCar.model = model;
+            infoCar.created = created;
+            infoCar.volume = volume;
+            infoCar.color = color;
+            return infoCar;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Car{"
+                + "brand='" + brand + '\''
+                + ", model='" + model + '\''
+                + ", created=" + created
+                + ", volume=" + volume
+                + ", color='" + color + '\''
+                + '}';
+    }
+
+    public static void main(String[] args) {
+        Car carCamry = new Builder()
+                .buildBrand("Toyota")
+                .buildModel("Camry")
+                .buildCreated(LocalDate.of(2021, 6, 1))
+                .buildVolume(2.5)
+                .buildColor("Red")
+                .build();
+        Car carFpace = new Builder()
+                .buildBrand("Jaguar")
+                .buildModel("F-Pace")
+                .buildVolume(3.0)
+                .buildColor("Blue")
+                .build();
+        System.out.println(carCamry);
+        System.out.println(carFpace);
+    }
+}
