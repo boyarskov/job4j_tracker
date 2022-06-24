3c2f8055e973e500ae9f66d0057853d24c69f8d6
Artem Boyarskov <arboyarskov@ya.ru>
2022-06-25 01:29:54 +0300

    0. Stream API улучшения [#58167]

--- src/main/java/ru/job4j/stream/Student.java
+++ src/main/java/ru/job4j/stream/Student.java
@@ -26,11 +26,15 @@ public class Student {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Student student = (Student) o;
-        return score == student.score &&
-                surname.equals(student.surname);
+        return score == student.score
+                && surname.equals(student.surname);
     }
 
     @Override
