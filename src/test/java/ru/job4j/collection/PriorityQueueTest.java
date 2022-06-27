381b692b40ae851f4e50b6f787c6e912afac2a2d
Artem Boyarskov <arboyarskov@ya.ru>
2022-06-27 14:28:32 +0300

    2. Local-Variable Type Inference [#70626 #316438]

--- src/test/java/ru/job4j/collection/PriorityQueueTest.java
+++ src/test/java/ru/job4j/collection/PriorityQueueTest.java
@@ -8,21 +8,21 @@ import static org.junit.Assert.*;
 public class PriorityQueueTest {
     @Test
     public void whenHigherPriority() {
-        PriorityQueue queue = new PriorityQueue();
+        var queue = new PriorityQueue();
         queue.put(new Task("low", 5));
         queue.put(new Task("urgent", 1));
         queue.put(new Task("middle", 3));
-        Task result = queue.take();
+        var result = queue.take();
         assertThat(result.getDesc(), is("urgent"));
     }
 
     @Test
     public void whenHigherPriority2() {
-        PriorityQueue queue = new PriorityQueue();
+        var queue = new PriorityQueue();
         queue.put(new Task("max", 3));
         queue.put(new Task("middle", 2));
         queue.put(new Task("min", 1));
-        Task result = queue.take();
+        var result = queue.take();
         assertThat(result.getDesc(), is("min"));
     }
 }
--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.assertTrue;
 public class PhoneDictionaryTest {
     @Test
     public void whenFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
@@ -20,7 +20,7 @@ public class PhoneDictionaryTest {
 
     @Test
     public void whenNotFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
