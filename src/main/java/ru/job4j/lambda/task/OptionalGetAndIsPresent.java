4133c607b0ea97e96775dd97b8bd6f3d9376b5af
Artem Boyarskov <arboyarskov@ya.ru>
2022-06-30 20:33:19 +0300

    5.2. Optional. Методы isPresent(), isEmpty(), get()

--- src/main/java/ru/job4j/lambda/task/OptionalGetAndIsPresent.java
+++ src/main/java/ru/job4j/lambda/task/OptionalGetAndIsPresent.java
@@ -5,14 +5,14 @@ import java.util.Optional;
 public class OptionalGetAndIsPresent {
     public static int get(int[] data, int el) {
         Optional index = OptionalGetAndIsPresent.indexOf(data, el);
-        if (OptionalGetAndIsPresent.indexOf(data, el).isPresent()) {
+        if (index.isPresent()) {
             return (int) index.get();
         }
         return -1;
     }
 
     private static Optional<Integer> indexOf(int[] data, int el) {
-                for (int i = 0; i < data.length; i++) {
+        for (int i = 0; i < data.length; i++) {
             if (data[i] == el) {
                 return Optional.of(i);
             }
