f565ef2a487fed39e639352e1aadb5d0583dafbf
Artem Boyarskov <arboyarskov@ya.ru>
2022-07-12 12:41:28 +0300

    5.5. Optional. Методы orElse(), orElseGet()

---
+++ src/main/java/ru/job4j/lambda/task/OptionalOrlElse.java
@@ -0,0 +1,9 @@
+package ru.job4j.lambda.task;
+
+import java.util.Optional;
+
+public class OptionalOrlElse {
+    public static Integer orElse(Optional<Integer> optional) {
+        return optional.orElse(-1);
+    }
+}
