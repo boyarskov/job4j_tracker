e1d2287d31a58d7ea2ce473de44f4006568a37d4
Artem Boyarskov <arboyarskov@ya.ru>
2022-07-25 17:29:28 +0300

    2.7. Сборка элементов. Сборка в строку

---
+++ src/test/java/ru/job4j/stream/task/JoiningMethodTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.stream.task;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.StringJoiner;
+
+import static org.junit.Assert.*;
+
+public class JoiningMethodTest {
+
+    @Test
+    public void test() {
+        StringJoiner expect = new StringJoiner(
+                System.lineSeparator(),
+                "Prefix",
+                "Suffix"
+        );
+        expect.add("A");
+        expect.add("B");
+        expect.add("C");
+        assertEquals(
+                expect.toString(),
+                JoiningMethod.join(List.of(
+                        "A", "B", "C"
+                ))
+        );
+    }
+}
