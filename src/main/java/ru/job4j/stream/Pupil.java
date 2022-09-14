04f7a60cf69143828029c7e428f9b7a5de896c28
Artem Boyarskov <arboyarskov@ya.ru>
2022-09-14 22:26:38 +0300

    upgrade jdk version

--- src/main/java/ru/job4j/stream/Pupil.java
+++ src/main/java/ru/job4j/stream/Pupil.java
@@ -1,7 +1,9 @@
 package ru.job4j.stream;
 
 import java.util.List;
-
+/*
 public record Pupil(String name, List<Subject> subjects) {
 //Record класс Pupil описывает ученика.
 }
+
+ */
--- src/main/java/ru/job4j/stream/Tuple.java
+++ src/main/java/ru/job4j/stream/Tuple.java
@@ -1,5 +1,8 @@
 package ru.job4j.stream;
-
+/*
 public record Tuple(String name, double score) {
     //Класс Tuple содержит результаты: имя и баллы. Этот класс используется как для учеников, так и для предметов.
 }
+
+
+ */
