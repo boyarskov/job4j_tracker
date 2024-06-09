5f865a1bc266c05f76fa02034d51dbb3b38ad512
Artem Boyarskov <arboyarskov@ya.ru>
2024-06-09 17:15:25 +0300

    11. Record [#504880 #500000]

---
+++ src/main/java/ru/job4j/format/Main.java
@@ -0,0 +1,21 @@
+package ru.job4j.format;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> data = null;
+        boolean result = true;
+
+        for (int i = 0; i < data.size(); i++) {
+            if (data.get(i)%2 != 0) {
+                result = false;
+            }
+        }
+
+
+        System.out.println(result);
+    }
+
+}
---
+++ src/main/java/ru/job4j/record/PersonRecord.java
@@ -0,0 +1,4 @@
+package ru.job4j.record;
+
+public record PersonRecord(String name, int age) {
+}
